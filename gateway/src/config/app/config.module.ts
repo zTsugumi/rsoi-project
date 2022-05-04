cfd238fb63bef607ab0e2cd154ef5333d5bcf770
zTsugumi <nguyenngochai.tin47@gmail.com>
2022-05-05 00:51:46 +0300

    GW: Init

---
+++ gateway/src/config/app/config.module.ts
@@ -0,0 +1,24 @@
+import * as Joi from 'joi';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfigService } from './config.service';
+import configuration from './configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      validationSchema: Joi.object({
+        APP_ENV: Joi.string().valid('development', 'production', 'test', 'provision'),
+        APP_NAME: Joi.string().default('gateway'),
+        PORT: Joi.number(),
+        HTTP_TIMEOUT: Joi.number(),
+        HTTP_MAX_REDIRECTS: Joi.number(),
+        URL_CONV: Joi.string(),
+      }),
+    }),
+  ],
+  providers: [ConfigService, AppConfigService],
+  exports: [AppConfigService],
+})
+export class AppConfigModule {}
---
+++ gateway/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppConfigService } from './config/app/config.service';
+import { APIPrefix } from './common/constant';
+import { HttpExceptionFilter } from './exception/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const appConfig: AppConfigService = app.get(AppConfigService);
+
+  app.setGlobalPrefix(APIPrefix.version);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(appConfig.port, () => {
+    Logger.log(`gateway is listening on ${appConfig.port}...`, 'Main');
+  });
+}
+bootstrap();
